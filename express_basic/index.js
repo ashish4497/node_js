fb2ef880db75900ffa87b92bbb229040b85274e5
ashish4497 <ak8393267@gmail.com>
2019-01-12 20:33:49 +0530

    node express start

---
+++ express_basic/index.js
@@ -0,0 +1,32 @@
+var  express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+const path = require('path');
+var ejs =('ejs')
+var app = express();
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
+// app.use(express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname ,'views'))
+app.set('view engine','views','ejs');
+
+app.use((req,res,next) => {
+  // console.log("cookies",new Date())
+  res.render('main.ejs',{he :"hay cool sachin"})
+  next()
+})
+
+app.get('/',(req,res)=>{
+  // res.send("start now!!");
+  // res.sendFile('index.html')
+  
+})
+app.post('/contact',(req,res)=>{
+  res.send(req.body);
+  // console.log(req.body)
+})
+
+app.listen(8000,()=>console.log("server start"));
