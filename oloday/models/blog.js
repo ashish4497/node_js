955774c1db6414d3f1c9019e73e0fae58d6855f2
ashish4497 <ak8393267@gmail.com>
2019-01-16 16:06:46 +0530

    create blogpost

---
+++ oloday/models/blog.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+  title : {type:String },
+  description : {type:String},
+  likes : {type:Number},
+  tag:[String],
+  date:{type: Date, default: Date.now()}
+}) 
+
+
+var blog = mongoose.model('blog',blogSchema);
+module.exports = blog;
